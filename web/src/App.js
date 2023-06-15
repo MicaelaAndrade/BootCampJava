b43ea8b320d245e305788779fc175cb076180bd4
Micaela Andrade <micaela.andrade@outlook.com>
2023-06-14 22:48:52 -0300

    chore: criando server / web

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import './styles/App.css';
+import './styles/reset.css';
+
+import { SideMenu } from './components/SideMenu/SideMenu';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu></SideMenu>
+      <h1>App Works!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/SideMenu/SideMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SideMenu.css';
+
+export const SideMenu = () => {
+  return (
+    <aside className="sideMenu">
+      <div className="sidemenu-button">
+        <span>+</span>
+        Novo Chat
+      </div>
+    </aside>
+  )
+}
+
+// Criando a base do CSS do APP - parou aqui
